dd6ff9bf451398c2a294d30de02c9726457e28b5
yeningzi <1772261575@qq.com>
2024-10-27 16:20:02 +0800

    项目初始结构创建

---
+++ guilmail-ware/src/main/java/com/atguigu/gulimall/ware/GuilimailWareApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.ware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuilimailWareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GuilimailWareApplication.class, args);
+    }
+
+}
